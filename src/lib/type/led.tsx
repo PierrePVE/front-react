e77e83f3353e0657048fffce22a5ae67bbcb6777
PierrePVE <pierre.vanelsuve@reseau.eseo.fr>
2025-01-29 10:10:40 +0100

    merge

--- src/lib/type/led.tsx
+++ src/lib/type/led.tsx
@@ -41,11 +41,8 @@ const Led = ({ value }) => {
 
   return (
     <div>
-      <button onClick={toggle} style={{ background: "transparent", border: "none", cursor: "pointer" }}>
-        <span
-          className="material-symbols-rounded"
-          style={{ color: isOn ? "yellow" : "#c890d0", fontSize: "48px" }}
-        >
+      <button onClick={toggle}>
+        <span className={`material-symbols-rounded ${isOn ? "light-on" : ""}`}>
           lightbulb
         </span>
       </button>
